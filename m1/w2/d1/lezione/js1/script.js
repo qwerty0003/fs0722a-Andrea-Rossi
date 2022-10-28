b8984f0f1b17bd5ec5b9b357a81cd34b8ae1fa26
Andrea Rossi <vinrossiandrea96@gmail.com>
2022-10-28 17:56:49 +0200

    lezione 1 w2

---
+++ m1/w2/d1/lezione/js1/script.js
@@ -0,0 +1,21 @@
+/**
+ * commento su piu righe
+ * 
+ * **/
+//commento ad una riga
+
+/**********************************************************
+ * Direttive di output
+ */
+
+// alert('mostra questa scritta')
+console.log('mostra questa scritta')
+console.log(3+2)
+console.log('%c mostra questa scritta', 'color:red;background-color:blue');
+
+document.getElementById('test').innerHTML = '<b>Prova</b>';
+
+document.write('Ciao') //potrebbe eliminare il contenuto della pagina, non si usa solitamente
+
+
+
