79ab1d7f7e93e180414306812d1284d68fea3885
Andrea Rossi <vinrossiandrea96@gmail.com>
2022-11-09 11:34:56 +0100

    esercizi w3d3

---
+++ m1/w3/d3/esercizio/script.js
@@ -0,0 +1,53 @@
+// creo la funzione di add
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("inputItem").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("lista").appendChild(li);
+    }
+    document.getElementById("inputItem").value = "";
+  
+    var span = document.createElement("span");
+    var testo = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(testo);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
+// Creo un bottone cancella vicino ad ogni elemento della lista
+var elementoLi = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < elementoLi.length; i++) {
+  var span = document.createElement("span");
+  var testo = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(testo);
+  elementoLi[i].appendChild(span);
+}
+
+// rendo reattivo il mio bottone cancella
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+// creo un simbolo check al click su un task
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'li') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
