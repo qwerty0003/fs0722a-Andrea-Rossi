1f75cd86dbf2b1a5ea6b48ff59f6204341b33489
Andrea Rossi <vinrossiandrea96@gmail.com>
2022-11-25 15:49:22 +0100

    esercizio_weeklyProject

---
+++ m2/w1/weekly project/assets/script.js
@@ -0,0 +1,22 @@
+
+window.addEventListener("scroll", function(event){
+    
+    var top = this.scrollY;
+
+    let navbar=this.document.querySelector("header");
+
+    let button=this.document.querySelector("#start");
+    
+    if(top>295)
+    { 
+        navbar.style.backgroundColor="#fff"
+
+        button.style.backgroundColor="#1a8917"
+    }
+    else
+    {
+        navbar.style.backgroundColor="#ffc017"
+        
+        button.style.backgroundColor="#191919"
+    }
+});
