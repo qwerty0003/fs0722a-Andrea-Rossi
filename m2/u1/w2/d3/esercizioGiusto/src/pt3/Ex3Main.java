6cc737d6357a3af519743b122a8b7b76c591f1ac
Andrea Rossi <vinrossiandrea96@gmail.com>
2023-02-08 18:04:57 +0100

    JavaW2D3#

--- m2/u1/w2/d3/esercizioGiusto/src/pt3/Ex3Main.java
+++ m2/u1/w2/d3/esercizioGiusto/src/pt3/Ex3Main.java
@@ -2,62 +2,112 @@ package pt3;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//TODO
 
 
 import pt1.Ex1Main;
 
 public class Ex3Main {
 
-	static Logger log = LoggerFactory.getLogger(Ex1Main.class);
+	
 	static Scanner sc = new Scanner(System.in);
 	static File file = new File("dirFile/test.txt");
-	static String[] elenco = new String[9];
-	
-	public static void main(String[] args) throws IOException {
+	static ArrayList<String> arr = new ArrayList<String>();
+	static ArrayList<String> arrWrite = new ArrayList<String>();
+	static String[] arrRecuperato = new String[100];
+	static HashMap<String,String> map = new HashMap<String,String>();
+
+	public static void main(String[] args) throws Exception {
+
+		System.out.println("CREO LISTA");
+		createList();
 		
-		arrayIt();
-				
+		System.out.println("LOG ARRAY CREATO E ARRAY CREATO DA SCRIVERE NEL FILE");
+		System.out.println(arr);
+		System.out.println(arrWrite);
 		
+		System.out.println("SCRIVO LISTA NEL FILE");
 		scriviFile();
-		String testoFile = leggiFile();
-		log.info(testoFile);
+		
+		System.out.println("LEGGO FILE");
+		String fileText = leggiFile();
+		System.out.println(fileText);
+		//RECUPERO DA FILE
+		recuperaDaFile();
+		//LOG ELEMENTI RECUPERATI DA FILE
+		System.out.println(map);
+		
+		
+		
+
+	}
+
+	public static void scriviFile() throws IOException {
+
+		for(int i = 0; i<arrWrite.size();i++) {
+			String str = arrWrite.get(i);
+			FileUtils.writeStringToFile(file, str, "utf-8", true);
+		}
 
 	}
 	
-	
-	public static void scriviFile(String str) throws IOException{
-		  
-		FileUtils.writeStringToFile(file, str,"utf-8",true);
-				
+	public static void recuperaDaFile() throws IOException {
+		String text = FileUtils.readFileToString(file, "UTF-8");
+		String[] lilMap = new String[1];
+		for(int i = 0;i<100;i++) {
+		if(text.contains("#"))
+			arrRecuperato = text.split("#");
+		}
+		for(int i = 0;i<=arrRecuperato.length-1;i++) {
+			lilMap = arrRecuperato[i].split("@");
+			map.put(lilMap[0], lilMap[1]);
+		}		
 	}
-	
+
 	public static String leggiFile() throws IOException {
-		String ciao = FileUtils.readFileToString(file,"UTF-8");
+		String ciao = FileUtils.readFileToString(file, "UTF-8");
 		return ciao;
 	}
-	
+
 	public static void cancellaFile() throws IOException {
 		FileUtils.delete(file);
+		System.out.println("File cancellato correttamente!");
+	}
+
+	public static void createList() {
+		try {
+		System.out.println("Quanti dipendenti vuoi creare?");
+		int n = Integer.parseInt(sc.nextLine());
+		for(int i =0;i<n;i++) {
+			System.out.println("Nome: ");
+			String nome = sc.nextLine();
+			System.out.println("Cognome: ");
+			String cognome = sc.nextLine();
+			System.out.println("Presenza settimanali (da 0 a 7): ");
+			int x = Integer.parseInt(sc.nextLine());
+			if(x >=0 && x <= 7) {
+				arr.add(nome+" "+cognome+" "+x);
+				arrWrite.add(nome+" "+cognome+"@"+x+"#");
+			}else
+				throw new Exception();
+		}
+		}catch(NumberFormatException ex) {
+			System.out.println("Un numero per favore, riprova!");
+			createList();
+		}catch(Exception ex) {
+			System.out.println("Un numero tra 0 e 7, riprova!");
+		}
 	}
 	
-	public static void arrayIt() {
-		elenco[0]="Operaio 1 = 2";
-		elenco[1]="Operaio 2 = 3";
-		elenco[2]="Operaio 3 = 4";
-		elenco[3]="Operaio 4 = 7";
-		elenco[4]="Operaio 5 = 0";
-		elenco[5]="Operaio 6 = 1";
-		elenco[6]="Operaio 7 = 2";
-		elenco[7]="Operaio 8 = 6";
-		elenco[8]="Operaio 9 = 4";
-		elenco[9]="Operaio 10 = 7";
+	public static void pulisciList() {
+		arr.clear();
+		arrWrite.clear();
 	}
 
 }
